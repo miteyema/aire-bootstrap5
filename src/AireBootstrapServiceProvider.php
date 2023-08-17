f39295b941b4040fc7bd0fedb9d0cddea3c20786
Tim <miteyema@users.noreply.github.com>
2023-08-17 10:58:52 +0700

    fix: bootstrap classes

--- src/AireBootstrapServiceProvider.php
+++ src/AireBootstrapServiceProvider.php
@@ -26,11 +26,11 @@ class AireBootstrapServiceProvider extends ServiceProvider
                 'group_append' => 'input-group-append',
                 'group_help_text' => 'form-text text-muted',
                 'group_errors' => '',
-                'label' => '',
+                'label' => 'form-label',
                 'input' => 'form-control',
 
-                'checkbox' => 'custom-control-input',
-                'checkbox_label' => 'custom-control-label',
+                'checkbox' => 'form-check-input',
+                'checkbox_label' => 'form-check-label',
                 'checkbox_wrapper' => 'custom-control custom-checkbox',
 
                 'checkbox_group' => 'custom-control-input',
